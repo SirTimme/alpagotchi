0720436e23e7e157b11d44f25a3792a0c74e4794
SirTimme <tim.piecho@gmail.com>
2020-12-07 15:35:11 +0100

    Update Config.java

--- src/main/java/Bot/Config.java
+++ src/main/java/Bot/Config.java
@@ -4,7 +4,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv
+            .configure()
+            .directory("../../../../Alpagotchi")
+            .load();
 
     public static String get(String key) {
         return dotenv.get(key);
