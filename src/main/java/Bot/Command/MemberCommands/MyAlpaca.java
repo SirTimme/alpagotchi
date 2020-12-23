b2b8edb48b43a50fbfbab91601ba1e358699a7ba
SirTimme <tim.piecho@gmail.com>
2020-12-23 11:38:33 +0100

    MyAlpaca Improved with Visual Bars

--- src/main/java/Bot/Command/MemberCommands/MyAlpaca.java
+++ src/main/java/Bot/Command/MemberCommands/MyAlpaca.java
@@ -7,44 +7,76 @@ import Bot.Config;
 import Bot.Database.IDataBaseManager;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
 
 public class MyAlpaca implements ICommand {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyAlpaca.class);
 
     @Override
     public void handle(CommandContext commandContext) {
-        final File myAlpacaFile = new File("src/main/resources/myAlpaca.gif");
+        final File alpacaStats = new File("src/main/resources/alpacaStats.jpg");
         final EmbedBuilder embedBuilder = new EmbedBuilder();
         final Member botCreator = commandContext.getGuild().getMemberById(Config.get("OWNER_ID"));
         final long memberID = commandContext.getMessage().getAuthor().getIdLong();
+
         final int hunger = IDataBaseManager.INSTANCE.getStatus(memberID, "hunger");
         final int thirst = IDataBaseManager.INSTANCE.getStatus(memberID, "thirst");
         final int energy = IDataBaseManager.INSTANCE.getStatus(memberID, "energy");
 
+        BufferedImage alpacaIMG = null;
+
+        try {
+            alpacaIMG = ImageIO.read(alpacaStats);
+
+        } catch (IOException error) {
+            LOGGER.error(error.getMessage());
+        }
+
+        if (alpacaIMG == null) {
+            LOGGER.error("Could not read alpaca file");
+            return;
+        }
+
+        Graphics alpakaGraphics = alpacaIMG.getGraphics();
+
+        alpakaGraphics.setColor(getColorOfValues(hunger));
+        alpakaGraphics.fillRect(31, 31, (int)(hunger * 1.49), 12);
+
+        alpakaGraphics.setColor(getColorOfValues(thirst));
+        alpakaGraphics.fillRect(31, 73, (int)(thirst * 1.49), 12);
+
+        alpakaGraphics.setColor(getColorOfValues(energy));
+        alpakaGraphics.fillRect(424, 31, (int)(energy * 1.49), 12);
+
+        alpakaGraphics.setColor(Color.GREEN);
+        alpakaGraphics.fillRect(424, 73, (int)(87 * 1.49), 12);
+
+        File newAlpacaFile = new File("src/main/resources/editedAlpacaStats.jpg");
+
+        try {
+            ImageIO.write(alpacaIMG, "jpg", newAlpacaFile);
+
+        } catch (IOException error) {
+            LOGGER.error(error.getMessage());
+        }
+
         embedBuilder.setTitle("" + IDataBaseManager.INSTANCE.getNickname(memberID) + "");
-        embedBuilder.addField("Hunger \uD83C\uDF56",  hunger + "/100", true);
-        embedBuilder.addField("Thirst \uD83C\uDF7C",  thirst + "/100", true);
-        embedBuilder.addField("Energy \uD83D\uDD0B",  energy + "/100", true);
+        embedBuilder.setDescription("_Hava a llamazing day!_");
         embedBuilder.setThumbnail(commandContext.getMember().getUser().getAvatarUrl());
         embedBuilder.setFooter("Created by " + botCreator.getEffectiveName(), botCreator.getUser().getEffectiveAvatarUrl());
         embedBuilder.setTimestamp(Instant.now());
-        embedBuilder.setImage("attachment://myAlpaca.gif");
+        embedBuilder.setImage("attachment://editedAlpacaStats.jpg");
 
-        if ((hunger + thirst + energy) / 3 >= 66) {
-            embedBuilder.setColor(Color.GREEN);
-
-        } else if ((hunger + thirst + energy) / 3 >= 33) {
-            embedBuilder.setColor(Color.YELLOW);
-
-        } else {
-            embedBuilder.setColor(Color.RED);
-        }
-
-        commandContext.getChannel().sendFile(myAlpacaFile, "myAlpaca.gif").embed(embedBuilder.build()).queue();
+        commandContext.getChannel().sendFile(newAlpacaFile, "editedAlpacaStats.jpg").embed(embedBuilder.build()).queue();
     }
 
     @Override
@@ -65,5 +97,22 @@ public class MyAlpaca implements ICommand {
     @Override
     public List<String> getAliases() {
         return List.of("ma", "stats", "alpaca");
+    }
+
+    private Color getColorOfValues(int value) {
+        if (value >= 80) {
+            return Color.GREEN;
+
+        } else if (value >= 60) {
+            return Color.YELLOW;
+
+        } else if (value >= 40) {
+            return Color.ORANGE;
+
+        } else if (value >= 20) {
+            return Color.RED;
+
+        } else
+            return Color.BLACK;
     }
 }
