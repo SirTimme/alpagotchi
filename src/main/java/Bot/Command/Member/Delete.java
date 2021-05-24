16cf3f74a4b91c272a02ba6393bd495f4d811443
SirTimme <tim.piecho@gmail.com>
2021-05-24 18:29:58 +0200

    Added missing PermissionCheck for init + delete command

--- src/main/java/Bot/Command/Member/Delete.java
+++ src/main/java/Bot/Command/Member/Delete.java
@@ -16,79 +16,79 @@ import java.util.EnumSet;
 import java.util.concurrent.TimeUnit;
 
 public class Delete implements ICommand {
-	private final EventWaiter waiter;
-	private final String[] emoteIDs = {"782229268914372609", "782229279312314368"};
+    private final EventWaiter waiter;
+    private final String[] emoteIDs = {"782229268914372609", "782229279312314368"};
 
-	public Delete(EventWaiter waiter) {
-		this.waiter = waiter;
-	}
+    public Delete(EventWaiter waiter) {
+        this.waiter = waiter;
+    }
 
-	@Override
-	public void execute(CommandContext ctx) {
-		final TextChannel channel = ctx.getChannel();
-		final long authorID = ctx.getAuthorID();
+    @Override
+    public void execute(CommandContext ctx) {
+        final TextChannel channel = ctx.getChannel();
+        final long authorID = ctx.getAuthorID();
 
-		if (IDatabase.INSTANCE.getUser(authorID) == null) {
-			channel.sendMessage(Error.NOT_INITIALIZED.getMessage(ctx.getPrefix(), getName())).queue();
-			return;
-		}
+        if (IDatabase.INSTANCE.getUser(authorID) == null) {
+            channel.sendMessage(Error.NOT_INITIALIZED.getMessage(ctx.getPrefix(), getName())).queue();
+            return;
+        }
 
-		channel.sendMessage("⚠ Are you sure you want to delete your data? You **permanently** lose all progress").queue((msg) -> {
-			msg.addReaction("GreenTick:" + emoteIDs[0]).queue();
-			msg.addReaction("RedCross:" + emoteIDs[1]).queue();
+        channel.sendMessage("⚠ Are you sure you want to delete your data? You **permanently** lose all progress").queue((msg) -> {
+            msg.addReaction("GreenTick:" + emoteIDs[0]).queue();
+            msg.addReaction("RedCross:" + emoteIDs[1]).queue();
 
-			waiter.waitForEvent(
-				GuildMessageReactionAddEvent.class,
-				(event) -> event.getMessageIdLong() == msg.getIdLong()
-					&& event.getMember().equals(ctx.getMember())
-					&& event.getReactionEmote().isEmote()
-					&& Arrays.asList(emoteIDs).contains(event.getReactionEmote().getEmote().getId()),
-				(event) -> {
-					final String emoteID = event.getReactionEmote().getEmote().getId();
-					msg.delete().queue();
+            waiter.waitForEvent(
+                    GuildMessageReactionAddEvent.class,
+                    (event) -> event.getMessageIdLong() == msg.getIdLong()
+                            && event.getMember().equals(ctx.getMember())
+                            && event.getReactionEmote().isEmote()
+                            && Arrays.asList(emoteIDs).contains(event.getReactionEmote().getEmote().getId()),
+                    (event) -> {
+                        final String emoteID = event.getReactionEmote().getEmote().getId();
+                        msg.delete().queue();
 
-					if (emoteID.equals(emoteIDs[0])) {
-						IDatabase.INSTANCE.deleteUser(authorID);
-						channel.sendMessage(Emote.GREENTICK + " Data successfully deleted").queue();
-					}
-					else {
-						channel.sendMessage(Emote.REDCROSS + " Delete process cancelled").queue();
-					}
-				},
-				90L, TimeUnit.SECONDS,
-				() -> {
-					msg.delete().queue();
-					channel.sendMessage(Emote.REDCROSS + " Answer timed out").queue();
-				}
-			);
-		});
-	}
+                        if (emoteID.equals(emoteIDs[0])) {
+                            IDatabase.INSTANCE.deleteUser(authorID);
+                            channel.sendMessage(Emote.GREENTICK + " Data successfully deleted").queue();
+                        } else {
+                            channel.sendMessage(Emote.REDCROSS + " Delete process cancelled").queue();
+                        }
+                    },
+                    90L, TimeUnit.SECONDS,
+                    () -> {
+                        msg.delete().queue();
+                        channel.sendMessage(Emote.REDCROSS + " Answer timed out").queue();
+                    }
+            );
+        });
+    }
 
-	@Override
-	public String getName() {
-		return "delete";
-	}
+    @Override
+    public String getName() {
+        return "delete";
+    }
 
-	@Override
-	public Level getLevel() {
-		return Level.MEMBER;
-	}
+    @Override
+    public Level getLevel() {
+        return Level.MEMBER;
+    }
 
-	@Override
-	public EnumSet<Permission> getCommandPerms() {
-		return EnumSet.of(
-			Permission.MESSAGE_WRITE,
-			Permission.MESSAGE_ADD_REACTION
-		);
-	}
+    @Override
+    public EnumSet<Permission> getCommandPerms() {
+        return EnumSet.of(
+                Permission.MESSAGE_WRITE,
+                Permission.MESSAGE_ADD_REACTION,
+                Permission.MESSAGE_HISTORY
+        );
+    }
 
-	@Override
-	public String getSyntax() {
-		return "delete";
-	}
+    @Override
+    public String getSyntax() {
+        return "delete";
+    }
 
-	@Override
-	public String getDescription() {
-		return "Deletes all of your data";
-	}
+    @Override
+    public String getDescription() {
+        return "Deletes all of your data";
+    }
 }
--- src/main/java/Bot/Command/Member/Init.java
+++ src/main/java/Bot/Command/Member/Init.java
@@ -21,104 +21,104 @@ import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("ConstantConditions")
 public class Init implements ICommand {
-	private final EventWaiter waiter;
-	private final String[] emoteIDs = {"782229268914372609", "782229279312314368"};
+    private final EventWaiter waiter;
+    private final String[] emoteIDs = {"782229268914372609", "782229279312314368"};
 
-	public Init(EventWaiter waiter) {
-		this.waiter = waiter;
-	}
+    public Init(EventWaiter waiter) {
+        this.waiter = waiter;
+    }
 
-	@Override
-	public void execute(CommandContext ctx) {
-		final TextChannel channel = ctx.getChannel();
-		final long authorID = ctx.getAuthorID();
-		final String prefix = ctx.getPrefix();
+    @Override
+    public void execute(CommandContext ctx) {
+        final TextChannel channel = ctx.getChannel();
+        final long authorID = ctx.getAuthorID();
+        final String prefix = ctx.getPrefix();
 
-		if (IDatabase.INSTANCE.getUser(authorID) != null) {
-			channel.sendMessage(Emote.REDCROSS + " You already own an alpaca").queue();
-			return;
-		}
+        if (IDatabase.INSTANCE.getUser(authorID) != null) {
+            channel.sendMessage(Emote.REDCROSS + " You already own an alpaca").queue();
+            return;
+        }
 
-		final User botCreator = ctx.getJDA().getUserById(Config.get("DEV_ID"));
-		final EmbedBuilder embed = new EmbedBuilder();
-		embed.setTitle("User information")
-			 .setDescription("Im glad, that Alpagotchi interests you and you want to interact with him.\nHere are two important points before you can start:")
-			 .setThumbnail(ctx.getJDA().getSelfUser().getAvatarUrl())
-			 .addField(
-				 "__§1 Storage of the UserID__",
-				 "Alpagotchi stores your personal Discord UserID in order to work, but this is public information and can be accessed by everyone",
-				 false
-			 )
-			 .addField(
-				 "__§2 Deletion of the UserID__",
-				 "If you change your mind about storing your UserID, use the `" + prefix + "delete` command to delete your data at any time",
-				 false
-			 )
-			 .setImage("https://cdn.discordapp.com/attachments/795637300661977132/811504330263625778/Reactions.png")
-			 .setFooter("Created by " + botCreator.getName(), botCreator.getEffectiveAvatarUrl())
-			 .setTimestamp(Instant.now());
+        final User botCreator = ctx.getJDA().getUserById(Config.get("DEV_ID"));
+        final EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("User information")
+                .setDescription("Im glad, that Alpagotchi interests you and you want to interact with him.\nHere are two important points before you can start:")
+                .setThumbnail(ctx.getJDA().getSelfUser().getAvatarUrl())
+                .addField(
+                        "__§1 Storage of the UserID__",
+                        "Alpagotchi stores your personal Discord UserID in order to work, but this is public information and can be accessed by everyone",
+                        false
+                )
+                .addField(
+                        "__§2 Deletion of the UserID__",
+                        "If you change your mind about storing your UserID, use the `" + prefix + "delete` command to delete your data at any time",
+                        false
+                )
+                .setImage("https://cdn.discordapp.com/attachments/795637300661977132/811504330263625778/Reactions.png")
+                .setFooter("Created by " + botCreator.getName(), botCreator.getEffectiveAvatarUrl())
+                .setTimestamp(Instant.now());
 
-		channel.sendMessage(embed.build()).queue((msg) -> {
-			msg.addReaction("GreenTick:" + emoteIDs[0]).queue();
-			msg.addReaction("RedCross:" + emoteIDs[1]).queue();
+        channel.sendMessage(embed.build()).queue((msg) -> {
+            msg.addReaction("GreenTick:" + emoteIDs[0]).queue();
+            msg.addReaction("RedCross:" + emoteIDs[1]).queue();
 
-			this.waiter.waitForEvent(
-				GuildMessageReactionAddEvent.class,
-				(event) -> event.getMessageIdLong() == msg.getIdLong()
-					&& event.getMember().equals(ctx.getMember())
-					&& event.getReactionEmote().isEmote()
-					&& Arrays.asList(emoteIDs).contains(event.getReactionEmote().getEmote().getId()),
-				(event) -> {
-					final String emoteID = event.getReactionEmote().getEmote().getId();
-					msg.delete().queue();
+            this.waiter.waitForEvent(
+                    GuildMessageReactionAddEvent.class,
+                    (event) -> event.getMessageIdLong() == msg.getIdLong()
+                            && event.getMember().equals(ctx.getMember())
+                            && event.getReactionEmote().isEmote()
+                            && Arrays.asList(emoteIDs).contains(event.getReactionEmote().getEmote().getId()),
+                    (event) -> {
+                        final String emoteID = event.getReactionEmote().getEmote().getId();
+                        msg.delete().queue();
 
-					if (emoteID.equals(emoteIDs[0])) {
-						IDatabase.INSTANCE.createUser(authorID);
-						channel.sendMessage(Emote.GREENTICK + " Your alpaca has been set up, use **" + prefix + "myalpaca** to see it").queue();
-					}
-					else {
-						channel.sendMessage(Emote.REDCROSS + " Init process cancelled").queue();
-					}
-				},
-				90L, TimeUnit.SECONDS,
-				() -> {
-					msg.delete().queue();
-					channel.sendMessage(Emote.REDCROSS + " Answer timed out").queue();
-				}
-			);
-		});
-	}
+                        if (emoteID.equals(emoteIDs[0])) {
+                            IDatabase.INSTANCE.createUser(authorID);
+                            channel.sendMessage(Emote.GREENTICK + " Your alpaca has been set up, use **" + prefix + "myalpaca** to see it").queue();
+                        } else {
+                            channel.sendMessage(Emote.REDCROSS + " Init process cancelled").queue();
+                        }
+                    },
+                    90L, TimeUnit.SECONDS,
+                    () -> {
+                        msg.delete().queue();
+                        channel.sendMessage(Emote.REDCROSS + " Answer timed out").queue();
+                    }
+            );
+        });
+    }
 
-	@Override
-	public String getName() {
-		return "init";
-	}
+    @Override
+    public String getName() {
+        return "init";
+    }
 
-	@Override
-	public Level getLevel() {
-		return Level.MEMBER;
-	}
+    @Override
+    public Level getLevel() {
+        return Level.MEMBER;
+    }
 
-	@Override
-	public List<String> getAliases() {
-		return List.of("setup");
-	}
+    @Override
+    public List<String> getAliases() {
+        return List.of("setup");
+    }
 
-	@Override
-	public EnumSet<Permission> getCommandPerms() {
-		return EnumSet.of(
-			Permission.MESSAGE_ADD_REACTION,
-			Permission.MESSAGE_WRITE
-		);
-	}
+    @Override
+    public EnumSet<Permission> getCommandPerms() {
+        return EnumSet.of(
+                Permission.MESSAGE_ADD_REACTION,
+                Permission.MESSAGE_WRITE,
+                Permission.MESSAGE_HISTORY
+        );
+    }
 
-	@Override
-	public String getSyntax() {
-		return "init";
-	}
+    @Override
+    public String getSyntax() {
+        return "init";
+    }
 
-	@Override
-	public String getDescription() {
-		return "Initializes your alpaca in the database";
-	}
+    @Override
+    public String getDescription() {
+        return "Initializes your alpaca in the database";
+    }
 }
