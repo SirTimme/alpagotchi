1c28c33b4ee5366bd677c2dc162dc1d360d660c5
SirTimme <tim.piecho@gmail.com>
2023-07-17 12:53:19 +0200

    Update LocaleConverter.java

--- src/main/java/bot/utils/LocaleConverter.java
+++ src/main/java/bot/utils/LocaleConverter.java
@@ -9,17 +9,11 @@ import java.util.Locale;
 public class LocaleConverter implements AttributeConverter<Locale, String> {
     @Override
     public String convertToDatabaseColumn(Locale locale) {
-        if (locale != null) {
-            return locale.toLanguageTag();
-        }
-        return null;
+        return locale.toLanguageTag();
     }
 
     @Override
     public Locale convertToEntityAttribute(String languageTag) {
-        if (languageTag != null && !languageTag.isEmpty()) {
-            return Locale.forLanguageTag(languageTag);
-        }
-        return null;
+        return Locale.forLanguageTag(languageTag);
     }
 }
