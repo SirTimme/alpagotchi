1607856e4dfaa7044646baa674365304fc826679
SirTimme <tim.piecho@gmail.com>
2023-09-02 12:07:20 +0200

    added LocaleUtils.java for locale specific utility methods

--- src/main/java/bot/commands/types/UserCommand.java
+++ src/main/java/bot/commands/types/UserCommand.java
@@ -2,9 +2,9 @@ package bot.commands.types;
 
 import bot.commands.ISlashCommand;
 import bot.db.IDatabase;
+import bot.localization.LocaleUtils;
 import bot.models.user.User;
 import bot.utils.Responses;
-import bot.utils.Utils;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 import java.util.Locale;
@@ -13,7 +13,7 @@ public abstract class UserCommand implements ISlashCommand {
     @Override
     public void execute(final SlashCommandInteractionEvent event) {
         final var user = IDatabase.INSTANCE.getUserById(event.getUser().getIdLong());
-        final var locale = Utils.retrieveLocale(event);
+        final var locale = LocaleUtils.getLocale(event);
 
         if (!event.getName().equals("init") && user == null) {
             event.reply(Responses.getLocalizedResponse("general.error.noAlpaca", locale)).setEphemeral(true).queue();
--- src/main/java/bot/utils/Utils.java
+++ src/main/java/bot/utils/Utils.java
@@ -1,39 +1,4 @@
 package bot.utils;
 
-import bot.db.IDatabase;
-import bot.models.guildsettings.GuildSettings;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
-
-import java.util.Locale;
-
 public class Utils {
-    public static Locale retrieveLocale(final SlashCommandInteractionEvent event) {
-        return retrieveLocale(event.getGuild());
-    }
-
-    public static Locale retrieveLocale(final StringSelectInteractionEvent event) {
-        return retrieveLocale(event.getGuild());
-    }
-
-    public static Locale retrieveLocale(final ButtonInteractionEvent event) {
-        return retrieveLocale(event.getGuild());
-    }
-
-    private static Locale retrieveLocale(final Guild guild) {
-        final var locale = Locale.ENGLISH;
-
-        if (guild == null) {
-            return locale;
-        }
-
-        final GuildSettings settings;
-        if ((settings = IDatabase.INSTANCE.getSettingsById(guild.getIdLong())) == null) {
-            return locale;
-        } else {
-            return settings.getLocale();
-        }
-    }
 }
