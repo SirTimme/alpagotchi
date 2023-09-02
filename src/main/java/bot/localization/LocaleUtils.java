755164f39f48a437472777bdcc8ee44ef8a2407e
Tim <46893185+SirTimme@users.noreply.github.com>
2023-09-02 11:25:27 +0200

    Refactored general `Utils` class into specialized util classes (#132)
    * refactor general util class into specific ones
    
    * removed invalid import directive

---
+++ src/main/java/bot/localization/LocaleUtils.java
@@ -0,0 +1,37 @@
+package bot.localization;
+
+import bot.db.IDatabase;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+
+import java.util.Locale;
+
+public class LocaleUtils {
+    public static Locale getLocale(final SlashCommandInteractionEvent event) {
+        return getLocale(event.getGuild());
+    }
+
+    public static Locale getLocale(final StringSelectInteractionEvent event) {
+        return getLocale(event.getGuild());
+    }
+
+    public static Locale getLocale(final ButtonInteractionEvent event) {
+        return getLocale(event.getGuild());
+    }
+
+    private static Locale getLocale(final Guild guild) {
+        final var locale = Locale.ENGLISH;
+        if (guild == null) {
+            return locale;
+        }
+
+        final var settings = IDatabase.INSTANCE.getSettingsById(guild.getIdLong());
+        if (settings == null) {
+            return locale;
+        }
+
+        return settings.getLocale();
+    }
+}
